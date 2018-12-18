38b0509322d98bc765d097721e7b393856a87a2b
osamtimizer <eaten.jb@gmail.com>
2018-12-19 00:53:15 +0900

    5-12 specified display function spec

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user:user_a) }
+
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
   describe '一覧表示機能' do
-      let(:user_a) { FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com') }
-      let(:user_b) { FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com') }
-
-    before do
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
-
     context 'ユーザAでログインしている時' do
       let(:login_user) { user_a }
 
@@ -29,4 +29,19 @@ describe 'タスク管理機能', type: :system do
       end
     end
   end
+
+  describe '詳細表示機能' do
+    context 'ユーザAがログインしている時' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+  end
+
 end
