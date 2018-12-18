efeed184b32e2b7e4e69d3950bf0b264ef13b569
osamtimizer <eaten.jb@gmail.com>
2018-12-19 00:37:07 +0900

    5-10 failure test

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -4,17 +4,14 @@ describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     before do
       user_a = FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com' )
-      user_b = FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com' )
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: 'a@example.com'
+      fill_in 'パスワード', with: 'password'
+      click_button 'ログインする'
     end
 
     context 'ユーザAでログインしている時' do
-      before do
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
 
       it 'ユーザAが作成したタスクが表示される' do
         expect(page).to have_content '最初のタスク'
@@ -23,10 +20,7 @@ describe 'タスク管理機能', type: :system do
 
     context 'ユーザBでログインしている時' do
       before do
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
+        user_b = FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com' )
       end
 
       it 'ユーザAが作成したタスクが表示されない' do
