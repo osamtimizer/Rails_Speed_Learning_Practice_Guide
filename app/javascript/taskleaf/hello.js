e56628ef80158723b4ee2de0b898760b2d4941a7
osamtimizer <eaten.jb@gmail.com>
2019-01-16 00:26:55 +0900

    8-4 yarn/webpacker

---
+++ app/javascript/taskleaf/hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    React.createElement('div', nill, 'Hello World'),
+    document.body.appendChild(document.createElement('div')),
+  );
+});
